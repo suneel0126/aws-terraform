7b41788913e0436228deb3390db618d7b80e4a2a
suneel <ssagi0126@gmail.com>
2023-04-06 18:28:07 +0530

    create-ec2-instance

---
+++ provider.tf
@@ -0,0 +1,59 @@
+terraform {
+
+}
+provider "aws" {
+  region = "us-east-1"
+  #access_key = ""
+  #secret_key = ""
+  #region = "us-west-2"
+}
+
+// Create ec2-instance
+resource "aws_instance" "ec2" {
+  //count         = 1
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key-name
+  subnet_id              = aws_subnet.subnet-public.id
+  vpc_security_group_ids = [aws_security_group.sg-01.id]
+  tags = {
+    "name" = "public"
+  }
+}
+
+
+// Create VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    "name" = "public"
+  }
+}
+
+// Create subnet
+resource "aws_subnet" "subnet-public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+}
+
+resource "aws_security_group" "sg-01" {
+  vpc_id = aws_vpc.main.id
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "value"
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    protocol    = "-1"
+    self        = false
+    to_port     = 0
+  }
+
+}
